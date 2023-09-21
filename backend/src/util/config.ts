0e59844bb331f189cc013fa9ccb23096660d29ae
Lassi Kuisma <lassi.kuisma1@gmail.com>
2023-09-21 19:03:20 +0300

    db migrations

---
+++ backend/src/util/config.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+
+const dbUrl = process.env.DATABASE_URL;
+if (!dbUrl) {
+  console.log('Database url missing');
+  process.exit(1);
+}
+
+// it seems that ts can't figure out that dbUrl is a string (due to process.exit)
+// using a second variable seems to fix this
+const DATABASE_URL = dbUrl;
+
+export { DATABASE_URL };
--- backend/src/util/db.ts
+++ backend/src/util/db.ts
@@ -1,20 +1,40 @@
 import { Sequelize } from 'sequelize';
-import 'dotenv/config';
+import { DATABASE_URL } from './config';
+import { Umzug, SequelizeStorage } from 'umzug';
 
-const dbUrl = process.env.DATABASE_URL;
-if (!dbUrl) {
-  console.log('database url not set');
-  process.exit(1);
-}
-
-const sequelize = new Sequelize(dbUrl);
+const sequelize = new Sequelize(DATABASE_URL);
 
 export const connectToDatabase = async () => {
   try {
     await sequelize.authenticate();
+    await runMigrations();
     console.log('connected to database');
   } catch (err) {
     console.log('failed to connect to database:', err);
     return process.exit(1);
   }
+};
+
+const migrationConf = {
+  migrations: {
+    glob: 'migrations/*.ts',
+  },
+  storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+  context: sequelize.getQueryInterface(),
+  logger: console,
+};
+
+export const migrator = new Umzug(migrationConf);
+export type Migration = typeof migrator._types.migration;
+
+const runMigrations = async () => {
+  const migrations = await migrator.up();
+  console.log('Migrations up to date', {
+    files: migrations.map((mig) => mig.name),
+  });
+};
+
+export const rollbackMigration = async () => {
+  await sequelize.authenticate();
+  await migrator.down();
 };
