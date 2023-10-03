9de6e526dfdd8a77ed96d066d1e98c44bd5cdeab
Lassi Kuisma <lassi.kuisma1@gmail.com>
2023-10-04 01:11:38 +0300

    frontend: separate component for country select box

---
+++ frontend/src/components/GameView/CountrySelect.tsx
@@ -0,0 +1,85 @@
+import {
+  Box,
+  Autocomplete,
+  TextField,
+  Button,
+  Alert,
+  CircularProgress,
+} from '@mui/material';
+import { useState } from 'react';
+import { Country, GameObject } from '../../types';
+
+interface Props {
+  game: GameObject;
+  submitMove: (country: Country) => void;
+}
+
+const CountrySelect = ({ game, submitMove }: Props) => {
+  const [countryValue, setCountryValue] = useState<null | Country>(null);
+  const [countryInput, setCountryInput] = useState('');
+  const [errorMessage, setErrorMessage] = useState<null | string>(null);
+
+  const guessClicked = () => {
+    setErrorMessage(null);
+
+    if (!countryValue) {
+      setErrorMessage('You need to select a country first!');
+      return;
+    }
+
+    submitMove(countryValue);
+
+    setCountryInput('');
+    setCountryValue(null);
+  };
+
+  return (
+    <Box width={600}>
+      <Box display={'flex'}>
+        <Autocomplete
+          id="country-select-combo-box"
+          sx={{ width: '100%' }}
+          value={countryValue}
+          onChange={(_event, newValue) => {
+            setCountryValue(newValue);
+          }}
+          inputValue={countryInput}
+          onInputChange={(_event, newInput) => {
+            setCountryInput(newInput);
+          }}
+          options={game.countries}
+          getOptionLabel={(option) => option.name}
+          isOptionEqualToValue={(option, value) => option.id === value.id}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Country"
+              inputProps={{
+                ...params.inputProps,
+              }}
+            />
+          )}
+        />
+        <Button variant="outlined" onClick={guessClicked}>
+          Guess
+        </Button>
+        <LoadingIcon visible={game.isSubmittingMove} />
+      </Box>
+      <ErrorDisplay errorMessage={errorMessage} />
+    </Box>
+  );
+};
+
+const ErrorDisplay = ({ errorMessage }: { errorMessage: null | string }) => {
+  return (
+    <Box sx={{ height: 50 }}>
+      {!!errorMessage && <Alert severity="error">{errorMessage}</Alert>}
+    </Box>
+  );
+};
+
+const LoadingIcon = ({ visible }: { visible: boolean }) => {
+  return <Box sx={{ width: 100 }}>{visible && <CircularProgress />}</Box>;
+};
+
+export default CountrySelect;
--- frontend/src/components/GameView/index.tsx
+++ frontend/src/components/GameView/index.tsx
@@ -1,16 +1,8 @@
-import {
-  Alert,
-  Autocomplete,
-  Box,
-  Button,
-  CircularProgress,
-  TextField,
-  Typography,
-} from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { Country, GameObject } from '../../types';
-import { useState } from 'react';
 import MoveList from './MoveList';
 import HintsView from './HintsViews';
+import CountrySelect from './CountrySelect';
 
 interface Props {
   game: undefined | GameObject;
@@ -18,80 +10,18 @@ interface Props {
 }
 
 const GameView = ({ game, submitMove }: Props) => {
-  const [countryValue, setCountryValue] = useState<null | Country>(null);
-  const [countryInput, setCountryInput] = useState('');
-  const [errorMessage, setErrorMessage] = useState<null | string>(null);
-
   if (!game) {
     return <div>Loading new game...</div>;
   }
 
-  const guessClicked = () => {
-    setErrorMessage(null);
-
-    if (!countryValue) {
-      setErrorMessage('You need to select a country first!');
-      return;
-    }
-
-    submitMove(countryValue);
-
-    setCountryInput('');
-    setCountryValue(null);
-  };
-
   return (
-    <div>
+    <Box>
       <Typography variant="h3">Guess a country</Typography>
-      <Box width={600}>
-        <Box display={'flex'}>
-          <Autocomplete
-            id="country-select-combo-box"
-            sx={{ width: '100%' }}
-            value={countryValue}
-            onChange={(_event, newValue) => {
-              setCountryValue(newValue);
-            }}
-            inputValue={countryInput}
-            onInputChange={(_event, newInput) => {
-              setCountryInput(newInput);
-            }}
-            options={game.countries}
-            getOptionLabel={(option) => option.name}
-            isOptionEqualToValue={(option, value) => option.id === value.id}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label="Country"
-                inputProps={{
-                  ...params.inputProps,
-                }}
-              />
-            )}
-          />
-          <Button variant="outlined" onClick={guessClicked}>
-            Guess
-          </Button>
-          <LoadingIcon visible={game.isSubmittingMove} />
-        </Box>
-        <ErrorDisplay errorMessage={errorMessage} />
-      </Box>
+      <CountrySelect game={game} submitMove={submitMove} />
       <HintsView hints={game.hints} />
       <MoveList moves={game.guesses} />
-    </div>
-  );
-};
-
-const ErrorDisplay = ({ errorMessage }: { errorMessage: null | string }) => {
-  return (
-    <Box sx={{ height: 50 }}>
-      {!!errorMessage && <Alert severity="error">{errorMessage}</Alert>}
     </Box>
   );
-};
-
-const LoadingIcon = ({ visible }: { visible: boolean }) => {
-  return <Box sx={{ width: 100 }}>{visible && <CircularProgress />}</Box>;
 };
 
 export default GameView;
