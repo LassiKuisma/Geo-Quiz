23b26147d7fc3b0bbd81ca23e0e395f46e27f914
Lassi Kuisma <lassi.kuisma1@gmail.com>
2023-11-06 16:47:30 +0200

    world map: display country name in tooltip

--- frontend/src/components/GameView/WorldMap.tsx
+++ frontend/src/components/GameView/WorldMap.tsx
@@ -4,6 +4,7 @@ import {
   Geography,
   ZoomableGroup,
 } from 'react-simple-maps';
+import { Tooltip } from 'react-tooltip';
 
 import { Country, GameMove } from '../../types/shared';
 
@@ -18,42 +19,50 @@ const WorldMap = ({ countries, guessed }: WorldMapProps) => {
   const colorUnknown = '#444444';
 
   return (
-    <ComposableMap>
-      <ZoomableGroup>
-        <Geographies geography="./ne_110m_admin_0_countries.geojson">
-          {({ geographies }) =>
-            geographies.map((geo) => {
-              const code = geo.properties.ISO_A3_EH;
+    <>
+      <ComposableMap>
+        <ZoomableGroup>
+          <Geographies geography="./ne_110m_admin_0_countries.geojson">
+            {({ geographies }) =>
+              geographies.map((geo) => {
+                const code = geo.properties.ISO_A3_EH;
 
-              const isGuessed = guessed.some(
-                (move) => move.guessedCountry.countryCode === code
-              );
+                const isGuessed = guessed.some(
+                  (move) => move.guessedCountry.countryCode === code
+                );
 
-              const regularCountry = countries.some(
-                (country) => country.countryCode === code
-              );
+                const regularCountry = countries.some(
+                  (country) => country.countryCode === code
+                );
 
-              const color = isGuessed
-                ? colorGuessed
-                : regularCountry
-                ? colorDefault
-                : colorUnknown;
+                const color = isGuessed
+                  ? colorGuessed
+                  : regularCountry
+                  ? colorDefault
+                  : colorUnknown;
 
-              return (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  style={{
-                    default: { fill: color },
-                    hover: { fill: '#04D' },
-                  }}
-                />
-              );
-            })
-          }
-        </Geographies>
-      </ZoomableGroup>
-    </ComposableMap>
+                const name = geo.properties.NAME_EN;
+
+                return (
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    style={{
+                      default: { fill: color },
+                      hover: { fill: '#04D' },
+                    }}
+                    data-tooltip-id="country-tooltip"
+                    data-tooltip-content={name}
+                    data-tooltip-float={true}
+                  />
+                );
+              })
+            }
+          </Geographies>
+        </ZoomableGroup>
+      </ComposableMap>
+      <Tooltip id="country-tooltip" />
+    </>
   );
 };
 
