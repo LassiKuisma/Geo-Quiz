b75b39a057a2c758f1c9161dedbe6ced57974483
Lassi Kuisma <lassi.kuisma1@gmail.com>
2024-03-05 19:40:03 +0200

    tests: country list filtering

--- tests/tests/country_list.spec.ts
+++ tests/tests/country_list.spec.ts
@@ -36,4 +36,48 @@ test.describe('country list', () => {
     const countryNames = new Set(await firstColumn.allTextContents());
     expect(countryNames).toEqual(expectedCountries);
   });
+
+  test('sorting by area', async ({ page }) => {
+    await page.getByRole('button', { name: 'Area' }).click();
+
+    // in our test data China is the biggest and Switzerland smallest by area
+
+    const topRowLocator = page.locator(
+      'table#country-table tbody tr:first-child td:first-child'
+    );
+    await expect(topRowLocator).toHaveText('China');
+
+    const bottomRowLocator = page.locator(
+      'table#country-table tbody tr:last-child td:first-child'
+    );
+    await expect(bottomRowLocator).toHaveText('Switzerland');
+  });
+
+  test('filtering by region', async ({ page }) => {
+    await page.getByRole('button', { name: 'Filters' }).click();
+    await page.getByLabel('Open').click();
+    await page
+      .getByRole('option', { name: 'Northern Europe' })
+      .getByRole('checkbox')
+      .check();
+
+    const countryCellsLocator = page.locator(
+      'table#country-table tbody tr td:first-child'
+    );
+
+    const expected = ['Finland', 'Norway', 'Sweden'];
+    await expect(countryCellsLocator).toHaveText(expected);
+  });
+
+  test('filtering by name', async ({ page }) => {
+    await page.getByRole('button', { name: 'Filters' }).click();
+    await page.getByLabel('Filter by name').fill('new zealand');
+
+    const countryCellsLocator = page.locator(
+      'table#country-table tbody tr td:first-child'
+    );
+
+    const expected = ['New Zealand'];
+    await expect(countryCellsLocator).toHaveText(expected);
+  });
 });
